6be878ad9f75b25c9aa0cc9775b35c6aeca92d9d
yankiweiss <yakovw2706@gmail.com>
2026-06-09 19:05:47 -0400

    update

--- api/routes/api/search.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const searchController = require('../../controllers/searchController');
-
-
-router.get('/',searchController.populateSelectSearch)
-
-module.exports = router;
