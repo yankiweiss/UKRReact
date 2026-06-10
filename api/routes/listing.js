f1af0e4cf25ba7e02ff847439e7c9d44cf02bf0f
yankiweiss <yakovw2706@gmail.com>
2026-06-09 20:08:50 -0400

    update

--- api/routes/listing.js
+++ api/routes/listing.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const listingController = require("../../controllers/listingController.js");
+const listingController = require("../controllers/listingController.js");
 
 
 router.route('/').get(listingController.getAllListing);
